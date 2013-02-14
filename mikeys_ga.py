d5a0f21f11eceb45d72a529aa2fb8c6a7680553b
Michael Simmonds <xbmc@filmon.com>
2013-02-14 15:34:56 +0000

    FOR BS

---
+++ mikeys_ga.py
@@ -0,0 +1,143 @@
+#INPUT BELOW INTO YOUR SETTINGS.XML
+#<setting id="ga_time" type="text" label="GA Called" default="2000-01-01 00:00:00" visible="false"/>
+#<setting id="visitor_ga" type="text" label="GA visitor" default="" visible="false"/>
+
+ADDON = xbmcaddon.Addon(id='plugin.YOUR PLUGIN')
+
+PATH = "PLUGIN NAME"  
+VERSION = "PLUGIN VERSION" 
+UATRACK="UA-xxxxxxxxx-1" #YOUR UA-ANALYTICS NUMBER
+VISITOR = ADDON.getSetting('visitor_ga')
+
+def parseDate(dateString):
+    try:
+        return datetime.datetime.fromtimestamp(time.mktime(time.strptime(dateString.encode('utf-8', 'replace'), "%Y-%m-%d %H:%M:%S")))
+    except:
+        return datetime.datetime.today() - datetime.timedelta(days = 1) #force update
+
+
+def checkGA():
+
+    secsInHour = 60 * 60
+    threshold  = 2 * secsInHour
+
+    now   = datetime.datetime.today()
+    prev  = parseDate(ADDON.getSetting('ga_time'))
+    delta = now - prev
+    nDays = delta.days
+    nSecs = delta.seconds
+
+    doUpdate = (nDays > 0) or (nSecs > threshold)
+    if not doUpdate:
+        return
+
+    ADDON.setSetting('ga_time', str(now).split('.')[0])
+    APP_LAUNCH()
+    
+    
+    
+                    
+def send_request_to_google_analytics(utm_url):
+    ua='Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3'
+    import urllib2
+    try:
+        req = urllib2.Request(utm_url, None,
+                                    {'User-Agent':ua}
+                                     )
+        response = urllib2.urlopen(req).read()
+    except:
+        print ("GA fail: %s" % utm_url)         
+    return response
+       
+def GA(group,name):
+        try:
+            try:
+                from hashlib import md5
+            except:
+                from md5 import md5
+            from random import randint
+            import time
+            from urllib import unquote, quote
+            from os import environ
+            from hashlib import sha1
+            utm_gif_location = "http://www.google-analytics.com/__utm.gif"
+            if not group=="None":
+                    utm_track = utm_gif_location + "?" + \
+                            "utmwv=" + VERSION + \
+                            "&utmn=" + str(randint(0, 0x7fffffff)) + \
+                            "&utmt=" + "event" + \
+                            "&utme="+ quote("5(channel*click*"+name+")")+\
+                            "&utmp=" + quote(PATH) + \
+                            "&utmac=" + UATRACK + \
+                            "&utmcc=__utma=%s" % ".".join(["1", VISITOR, VISITOR, VISITOR,VISITOR,"2"])
+                    try:
+                        print "============================ POSTING TRACK EVENT ============================"
+                        send_request_to_google_analytics(utm_track)
+                    except:
+                        print "============================  CANNOT POST TRACK EVENT ============================" 
+            if name=="None":
+                    utm_url = utm_gif_location + "?" + \
+                            "utmwv=" + VERSION + \
+                            "&utmn=" + str(randint(0, 0x7fffffff)) + \
+                            "&utmp=" + quote(PATH) + \
+                            "&utmac=" + UATRACK + \
+                            "&utmcc=__utma=%s" % ".".join(["1", VISITOR, VISITOR, VISITOR, VISITOR,"2"])
+            else:
+                if group=="None":
+                       utm_url = utm_gif_location + "?" + \
+                                "utmwv=" + VERSION + \
+                                "&utmn=" + str(randint(0, 0x7fffffff)) + \
+                                "&utmp=" + quote(PATH+"/"+name) + \
+                                "&utmac=" + UATRACK + \
+                                "&utmcc=__utma=%s" % ".".join(["1", VISITOR, VISITOR, VISITOR, VISITOR,"2"])
+                else:
+                       utm_url = utm_gif_location + "?" + \
+                                "utmwv=" + VERSION + \
+                                "&utmn=" + str(randint(0, 0x7fffffff)) + \
+                                "&utmp=" + quote(PATH+"/"+group+"/"+name) + \
+                                "&utmac=" + UATRACK + \
+                                "&utmcc=__utma=%s" % ".".join(["1", VISITOR, VISITOR, VISITOR, VISITOR,"2"])
+                                
+            print "============================ POSTING ANALYTICS ============================"
+            send_request_to_google_analytics(utm_url)
+            
+        except:
+            print "================  CANNOT POST TO ANALYTICS  ================" 
+            
+            
+def APP_LAUNCH():
+        print '==========================   '+PATH+' '+VERSION+'   =========================='
+        try:
+            try:
+                from hashlib import md5
+            except:
+                from md5 import md5
+            from random import randint
+            import time
+            from urllib import unquote, quote
+            from os import environ
+            from hashlib import sha1
+            import platform
+            VISITOR = ADDON.getSetting('visitor_ga')
+            try: 
+                PLATFORM=platform.system()+' '+platform.release()
+            except: 
+                PLATFORM=platform.system()
+            utm_gif_location = "http://www.google-analytics.com/__utm.gif"
+            utm_track = utm_gif_location + "?" + \
+                    "utmwv=" + VERSION + \
+                    "&utmn=" + str(randint(0, 0x7fffffff)) + \
+                    "&utmt=" + "event" + \
+                    "&utme="+ quote("5(app*launch*"+PLATFORM+")")+\
+                    "&utmp=" + quote(PATH) + \
+                    "&utmac=" + UATRACK + \
+                    "&utmcc=__utma=%s" % ".".join(["1", VISITOR, VISITOR, VISITOR,VISITOR,"2"])
+            try:
+                print "============================ POSTING APP LAUNCH TRACK EVENT ============================"
+                send_request_to_google_analytics(utm_track)
+            except:
+                print "============================  CANNOT POST APP LAUNCH TRACK EVENT ============================" 
+            
+        except:
+            print "================  CANNOT POST TO ANALYTICS  ================" 
+checkGA()
